dcc72c4b7115d7cfe92f28d2e5405dcfe759f014
k6407399 <kondruravikumar@outlook.com>
2025-01-01 15:30:50 +0000

    Initial commit for User Frontend

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h1>Welcome to Realtor</h1>
+    <p>Explore properties without signing up!</p>
+  </div>
+);
+
+export default Home;
